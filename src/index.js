33292bb72906991cb2d957d3c1e918fa2789d176
RM <rishikamittal2005@gmail.com>
2025-05-08 16:42:11 +0530

    add user n video model

--- src/index.js
+++ src/index.js
@@ -6,7 +6,15 @@ dotenv.config({
 })
 
 connectDB() 
-
+.then(()=>{
+    application.listen(process.env.PORT || 8000)=>{
+        console.log(`Server is running at port: ${process.env.PORT}`)
+    }
+}    
+)
+.catch((err)=>{
+    console.log("MongoDB connection failed!!",err)
+})
 
 // ( async () =>{
 //     try{
