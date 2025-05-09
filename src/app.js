2c1090df773270fe15ebd4197a31527484056a9f
RM <rishikamittal2005@gmail.com>
2025-05-09 15:18:51 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -10,10 +10,16 @@ app.use(cors({
     credentials: true
 }))
 
+//Middleware config
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended:true, limit:"16kb"}))
 app.use(express.static("public"))
 
 app.use(cookieParser())
 
-export(app)
+import userRouter from "./routes/user.routes.js"
+
+//rotes declaration
+app.use("/api/v1/users",userRouter)              // http://localhost:8000/api/v1/users/register
+
+export {app}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js"
+import {app} from "./app.js"
 
 dotenv.config({
     path: './env'
@@ -7,9 +8,9 @@ dotenv.config({
 
 connectDB() 
 .then(()=>{
-    application.listen(process.env.PORT || 8000)=>{
+    app.listen(process.env.PORT || 8000, () => {
         console.log(`Server is running at port: ${process.env.PORT}`)
-    }
+    })
 }    
 )
 .catch((err)=>{
